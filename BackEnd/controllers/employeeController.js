a16b6c366eeb64ce8edf390352c59bdf09bdad05
AnilShebin <anilshebin@gmail.com>
2023-12-01 10:29:45 +0530

    Backend: Refactor CRUD operations to use employeeID instead of _id
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- BackEnd/controllers/employeeController.js
+++ BackEnd/controllers/employeeController.js
@@ -1,90 +1,94 @@
 const asyncHandler = require("express-async-handler");
 const Employee = require("../models/employeeModel");
 
-//Get all employees
-//route Get /api/employees
-//access public
-const getEmployees = asyncHandler (async (req, res) => {
+// Get all employees
+// route Get /api/employees
+// access public
+const getEmployees = asyncHandler(async (req, res) => {
   const employees = await Employee.find();
   res.status(200).json(employees);
 });
 
-//Create New employees
-//route Post /api/employees
-//access public
-const createEmployee = asyncHandler (async (req, res) => {
+// Create New employees
+// route Post /api/employees
+// access public
+const createEmployee = asyncHandler(async (req, res) => {
   console.log("The request body is:", req.body);
-  const{employeeID, employeeName, employeeAge, employeeGender, employeeDOJ, employeeRemarks, employeeAcuredLeaves} = req.body;
-  /* if(!employeeID || !employeeName || !employeeAge || !employeeGender ||!employeeDOJ || !employeeRemarks || !employeeAcuredLeaves){
-    res.status(400);
-    throw new Error("All fields are mandatory !");
-  } */
+  const {
+    employeeID,
+    employeeName,
+    employeeAge,
+    employeeGender,
+    employeeDOJ,
+    employeeRemarks,
+    employeeAcuredLeaves
+  } = req.body;
 
   const employee = await Employee.create({
-    employeeID, 
-    employeeName, 
-    employeeAge, 
-    employeeGender, 
-    employeeDOJ, 
-    employeeRemarks, 
+    employeeID,
+    employeeName,
+    employeeAge,
+    employeeGender,
+    employeeDOJ,
+    employeeRemarks,
     employeeAcuredLeaves
   });
 
   res.status(201).json(employee);
 });
 
-
-//Get all employees
-//route Get /api/employees/:id
-//access public
-const getEmployee = asyncHandler (async (req, res) => {
-  const employee = await Employee.findById(req.params.id);
-  if(!Employee){
+// Get individual employees
+// route Get /api/employees/:id
+// access public
+const getEmployee = asyncHandler(async (req, res) => {
+  const employee = await Employee.findOne({ employeeID: req.params.id });
+  if (!employee) {
     res.status(404);
     throw new Error("Employee not found");
   }
   res.status(200).json(employee);
 });
 
-
-//Update all employees
-//route Post /api/employees
-//access public
-const updateEmployee = asyncHandler (async (req, res) => {
-  const employee = await Employee.findById(req.params.id);
-  if(!employee){
+// Update all employees
+// route Post /api/employees
+// access public
+const updateEmployee = asyncHandler(async (req, res) => {
+  const employee = await Employee.findOne({ employeeID: req.params.id });
+  if (!employee) {
     res.status(404);
     throw new Error("Employee not found");
   }
 
-  const updatedEmployee = await Employee.findByIdAndUpdate(
-    req.params.id,
+  const updatedEmployee = await Employee.findOneAndUpdate(
+    { employeeID: req.params.id },
     req.body,
-    { new: true}
+    { new: true }
   );
-  
+
   res.status(200).json(updatedEmployee);
-  
 });
 
-
-//Delete all employees
-//route Delete /api/employees/:id
-//access public
-const deleteEmployee = asyncHandler (async (req, res) => {
-  const employee = await Employee.findById(req.params.id);
-  if(!employee){
-    res.status(404);
-    throw new Error("Employee not found");
+// Delete all employees
+// route Delete /api/employees/:id
+// access public
+const deleteEmployee = async (req, res) => {
+  try {
+    const deleteEmployee = await Employee.findOneAndDelete({
+      employeeID: req.params.id
+    });
+    if (!deleteEmployee) {
+      return res.status(404).json({ message: 'Employee not found' });
+    }
+    res.status(200).json({ message: 'Employee deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  await Employee.remove();
-  res.status(200).json(employee);
-});
+};
 
-
-module.exports ={
-  getEmployees, 
-  createEmployee, 
-  getEmployee, 
-  updateEmployee, 
-  deleteEmployee};
+module.exports = {
+  getEmployees,
+  createEmployee,
+  getEmployee,
+  updateEmployee,
+  deleteEmployee
+};
--- BackEnd/routes/employeeRoutes.js
+++ BackEnd/routes/employeeRoutes.js
@@ -14,23 +14,3 @@ router.route("/").get(getEmployees).post(createEmployee);
 router.route("/:id").get(getEmployee).put(updateEmployee).delete(deleteEmployee);
 
 module.exports = router;
-
-
-
-/*router.route("/").get((req,res) =>{
-  res.status(200).json({message: "Get all Employees"});
-});
-router.route("/").post((req,res) =>{
-  res.status(200).json({message: "Create Employees"});
-});
-router.route("/:id").get((req,res) =>{
-  res.status(200).json({message: `Get Employees for ${req.params.id}`});
-});
-router.route("/:id").put((req,res) =>{
-  res.status(200).json({message: `Update Employees for ${req.params.id}`});
-});
-router.route("/:id").delete((req,res) =>{
-  res.status(200).json({message:  `Update Employees for ${req.params.id}`});
-});
-
-*/
